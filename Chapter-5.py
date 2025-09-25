e88d2b6908dd8454397145d714d5026ccbc5630c
Precipitation-Rain <rajvardhanshewale1200@gmail.com>
2025-09-25 22:32:46 +0530

    Build currency converter app

--- Chapter-5.py
+++ Chapter-5.py
@@ -1,1 +1,21 @@
 #Deal with api and requests
+
+import  streamlit as st
+import requests
+
+st.title("Live Currency Converter")
+amount = st.number_input("Enter amount in INR",min_value=1)
+
+target_currency = st.selectbox("Choose the Currency",["USD","EUR","GBP","JPY"])
+
+if st.button("Convert"):
+    url = "https://api.exchangerate-api.com/v4/latest/INR"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        rate = data["rates"][target_currency]
+        converted = rate * amount
+        st.success(f"{amount} INR = {converted:.2f} {target_currency}")
+    else:
+        st.error("Falied to fetch conversion rate")
